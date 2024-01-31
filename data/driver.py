e6423d46afbf66685fedf65d10c07bc75fa2f4fb
cueltschey <chaseuelt@gmail.com>
2024-01-31 14:42:04 -0600

    working on web

--- data/driver.py
+++ data/driver.py
@@ -3,38 +3,17 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+def create_chrome_driver(url, detach=True):
+    options = Options()
+    if not detach:
+        options.add_experimental_option("detach",False)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
+    return driver
+#scroll_script = "window.scrollTo(0, document.body.scrollHeight);"
 
-#options = Options()
-#options.add_experimental_option("detach",True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://happygardens.com/collections/wind-spinners-whirligigs?usf_take=56")
-products_found = list()
-
-scroll_script = "window.scrollTo(0, document.body.scrollHeight);"
-
-def get_items():
-    items = driver.find_elements("xpath", "//div[@class='productitem']")
-
-
-    for item in items:
-        try:
-            price_and_name = item.text.split("\n")
-            if len(price_and_name) >= 4:
-                name = price_and_name[3]
-                price = price_and_name[2]
-            else:
-                name = price_and_name[2]
-                price = price_and_name[1]
-            html_string = str(item.get_attribute("innerHTML"))
-            soup = BeautifulSoup(html_string, "html.parser")
-            img_url = "https:" + soup.find_all('img')[0].get('srcset')
-            products_found.append((name, price, img_url))
-        except:
-            print(item.text.split("\n"))
-
-for _ in range(2):
-    driver.execute_script(scroll_script)
-    time.sleep(3)
-    get_items()
+#items = driver.find_elements("xpath", "//div[@class='productitem']")
+#item.get_attribute("innerHTML")
+#driver.execute_script(scroll_script)
 
 
--- data/inaturalist.py
+++ data/inaturalist.py
@@ -1,5 +1,19 @@
-from webscraper import Driver, Downloader, HTMLParser
+from utils import Downloader, HTMLParser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from driver import create_chrome_driver
+import time
+
+
 
 downloader = Downloader()
-driver = Driver()
-html_parser = HTMLParser()
+driver = create_chrome_driver("https://www.inaturalist.org/observations?place_id=any&q=microorganism&subview=table")
+time.sleep(5)
+
+image_links = driver.find_elements("xpath", "//a[@class='img  ']")
+name_links = driver.find_elements("xpath", "//a[@class='display-name sciname' and @ng-show='taxon.id']")
+
+print(len(image_links))
+print(len(name_links))
