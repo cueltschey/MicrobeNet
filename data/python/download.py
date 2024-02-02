b01ed3af4a54c25479e370490e1df09e1522f9a7
cueltschey <chaseuelt@gmail.com>
2024-02-02 12:41:12 -0600

    finalized download script

--- data/python/download.py
+++ data/python/download.py
@@ -1,24 +1,32 @@
 import json
 from utils import Downloader
 import os
+import random
 
 d = Downloader()
 results = list()
 
-with open("inaturalist.json", "r") as f:
-    results = json.load(f)
+json_path = "C:\\Users\\paige\\Desktop\\microbe-id\\data"
+
+def download_images(filename):
+    with open(os.path.join(json_path, filename), "r") as f:
+        results = json.load(f)
 
 
-desk_path = "C:\\Users\\paige\\Desktop\\microbe_images"
-count = 200
-for item in results:
-    count += 1
-    genus = ""
-    if item["name"].split(" ")[0] == "Genus":
-        genus = item["name"].split(" ")[1]
-    else:
-        genus = item["name"].split(" ")[0]
+    desk_path = "C:\\Users\\paige\\Desktop\\microbe_images"
+    count = random.randint(0,200) * 100
+    for item in results:
+        count += 1
+        genus = ""
+        if item["name"].split(" ")[0] == "Genus":
+            genus = item["name"].split(" ")[1]
+        else:
+            genus = item["name"].split(" ")[0]
+        print(f"Dowloading inaturalist_{count}.jpg into folder {os.path.join(desk_path, genus)}")
 
-    d.download_image(item["image_url"][1:-1], os.path.join(desk_path, genus), f"inaturalist_{count}.jpg")
-    
+        d.download_image(item["image_url"][1:-1], os.path.join(desk_path, genus), f"inaturalist_{count}.jpg")
+        
+for filename in os.listdir(json_path):
+    download_images(filename)
+
 
