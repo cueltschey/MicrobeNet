776ad81e7e6556017105cadadbb63c189b08d872
cueltschey <chaseuelt@gmail.com>
2024-02-13 08:09:10 -0600

    finished new scraping

--- data_collection/realmicro.py
+++ data_collection/realmicro.py
@@ -1,6 +1,7 @@
 from driver import create_chrome_driver
 import time
 import json
+import os
 
 primary_url = "https://realmicrolife.com/all-procaryonts/"
 
@@ -14,7 +15,7 @@ def get_page_links(url):
     for _ in range(scroll_chunks):
         time.sleep(1)
         new_links = driver.find_elements("xpath", "//a[@class='pgcsimplygalleryblock-justified-item-button']")
-        page_links += [link for link in new_links if link not in page_links]
+        page_links += [link.get_attribute("href") for link in new_links if link.get_attribute("href") not in page_links]
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     driver.quit()
     return page_links
@@ -22,11 +23,15 @@ def get_page_links(url):
 def get_images(url):
     driver = create_chrome_driver(url)
     results = list()
-    for item in driver.find_elements("xpath", "//img[contains(@class, 'size-full')]"):
-        new_entry = dict()
-        new_entry["name"] = item.get_attribute("alt").split("-")[0]
-        new_entry["image_url"] = item.get_attribute("src")
-        print(new_entry)
+    time.sleep(4)
+    for _ in range(4):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        for item in driver.find_elements("xpath", "//img[contains(@class, 'size-full')]"):
+            new_entry = dict()
+            new_entry["name"] = item.get_attribute("alt").split("-")[0]
+            new_entry["image_url"] = item.get_attribute("src")
+            results.append(new_entry)
+            print(new_entry["name"], " --> ", new_entry["image_url"])
     driver.quit()
     return results
 
@@ -35,10 +40,9 @@ links = get_page_links(primary_url)
 
 count = 0
 for link in links:
-    print(link)
-    print(link.get_attribute("href"))
-    #results = get_images(link.get_attribute("href"))
-    results = list()
+    count += 1
+    print("Visiting link: ", link)
+    results = get_images(link)
     with open(os.path.join("/mnt/c/Users/paige/Desktop/realmicrolife", f"real_{count}.json"), "w") as f:
         json.dump(results, f, indent=4)
 
