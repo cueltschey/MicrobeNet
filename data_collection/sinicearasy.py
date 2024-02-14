5f75f5c31fb27fefc5243e83e091d6b8b5478e09
cueltschey <chaseuelt@gmail.com>
2024-02-14 14:39:29 -0600

    finished sinicearasy scraping

--- data_collection/sinicearasy.py
+++ data_collection/sinicearasy.py
@@ -4,26 +4,48 @@ import json
 import time
 
 
-driver = create_chrome_driver("http://galerie.sinicearasy.cz/galerie/akce/2001/specialni-fykologie-2001")
 
 output_path = "/mnt/c/Users/paige/Desktop/sinicearasy"
 
-def collect_images(genus):
-    images = [{"name":genus, "image_url":element.get_attribute("src")} for element in driver.find_elements("xpath", "//img")[1:]]
+def collect_images(url):
+    internal_driver = create_chrome_driver(url)
+    time.sleep(5)
+    genus = internal_driver.find_element("xpath", "//h1[@class='pageTitle']").text
+    images = [{"name":genus, "image_url":"http://galerie.sinicearasy.cz/" + element.get_attribute("data-src")} for element in internal_driver.find_elements("xpath", "//img[contains(@data-src, '/')]")]
+    internal_driver.quit()
+    print(genus, ":\n")
+    if len(images) == 0:
+        return
     for image in images:
         print(image["name"], " --> ", image["image_url"])
+    if os.path.exists(os.path.join(output_path, genus+ ".json")):
+        previous_images = list()
+        with open(os.path.join(output_path, genus + ".json"), "r") as f:
+            print("Reading in: ", genus + ".json")
+            previous_images = json.load(f)
+        images += previous_images
+
     with open(os.path.join(output_path, genus + ".json"), "w") as f:
+        print("Writing...")
         json.dump(images, f, indent=4)
 
+def get_urls():
+    driver = create_chrome_driver("http://galerie.sinicearasy.cz/galerie/")
+    time.sleep(5)
+    urls = [element.get_attribute("href") for element in driver.find_elements("xpath", "//a[contains(@class, 'node_id')]")]
+    driver.quit()
+    return urls
 
-num_links = len(driver.find_elements("xpath", "//li[contains(@class, 'doc')]"))
+urls = get_urls()
+print(f"Found {len(urls)} links")
 
-for index in num_links:
+for url in urls:
     try:
-        link = driver.find_elements("xpath", "li//[contains(@class, 'doc')]")[index]
-        link.click()
-        genus = driver.find_elements("xpath", "//h1[@class='page-title']")[0]
-        collect_images(genus)
+        if "akce" in url:
+            print("skip ", url)
+            continue
+        print("Visiting: ", url)
+        collect_images(url)
 
-    except: 
-        print("Failed")
+    except Exception as e: 
+        print(e)
