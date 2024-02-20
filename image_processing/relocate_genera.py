0c3ce248810096933a2dc63e319c11f6aea7718d
Charles Ueltschey <chaseuelt@gmail.com>
2024-02-19 21:38:59 -0600

    relocation complete

--- image_processing/relocate_genera.py
+++ image_processing/relocate_genera.py
@@ -11,14 +11,31 @@ good = 0
 bad = 0
 
 def sort_by_category(dirname, category):
+    successful = 0
+    size = len(os.listdir(dirname))
     for file in os.listdir(dirname):
+        if "phylum" in file:
+            sort_by_category(os.path.join(dirname, file), "class")
         try:
-            if not os.path.exists(os.path.join(dirname, info[file][category])):
-                os.mkdir(os.path.join(dirname, info[file][category]))
-            os.rename(os.path.join(dirname, file), os.path.join(dirname, info[file][category], file))
+            if not os.path.exists(os.path.join(dirname, f"{category}_" + info[file][category])):
+                os.mkdir(os.path.join(dirname, f"{category}_" + info[file][category]))
+            os.rename(os.path.join(dirname, file), os.path.join(dirname, f"{category}_" + info[file][category], file))
             print(file, "moved")
+            successful += 1
         except:
             print("fail")
+    print(f"Stats: {(successful / size) * 100}%")
 
 
-# sort_by_category("/home/chasuelt/Desktop/Backup_Images/Eukaryota/", "kingdom")
+def unpack_dir(dirname):
+    size = len(os.listdir(dirname))
+    for file in os.listdir(dirname):
+        print(file)
+        for subfile in os.listdir(os.path.join(dirname, file)):
+            if os.path.isdir(os.path.join(dirname, file, subfile)):
+                print(file, "moved")
+                os.rename(os.path.join(dirname, file, subfile), os.path.join(dirname, file, "..", subfile))
+
+sort_by_category("/home/chasuelt/Desktop/Backup_Images/superkingdom_Eukaryota/", "class")
+#unpack_dir("/home/chasuelt/Desktop/Backup_Images/Eukaryota")
+#unpack_dir("/home/chasuelt/Desktop/Backup_Images/")
